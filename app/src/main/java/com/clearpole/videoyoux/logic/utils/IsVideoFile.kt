c710dfb2d1e703935850a35c5095801224932cd7
clearpole <clearpole@qq.com>
2023-03-12 20:20:20 +0800

    修改了代码
    Signed-off-by: clearpole <clearpole@qq.com>

--- app/src/main/java/com/clearpole/videoyoux/logic/utils/IsVideoFile.kt
+++ app/src/main/java/com/clearpole/videoyoux/logic/utils/IsVideoFile.kt
@@ -7,41 +7,49 @@ import kotlinx.coroutines.withContext
 class IsVideoFile {
     companion object {
         suspend fun start(any: String): Boolean {
-            var allow = false
-            var index = -1
-            withContext(Dispatchers.IO) {
+            return withContext(Dispatchers.IO) {
                 val suffix = any.substring(any.lastIndexOf(".") + 1, any.length)
-                val allowSuffix = arrayListOf(
-                    "AVI",
-                    "FLV",
-                    "MOV",
-                    "MKV",
-                    "MP4",
-                    "MPEG",
-                    "MPG",
-                    "VOB",
-                    "WMV",
-                    "3GP",
-                    "ASF",
-                    "DIVX",
-                    "F4V",
-                    "M4V",
-                    "WEBM",
-                    "MXF",
-                    "OGG",
-                    "TS",
-                    "M2TS",
-                    "SWF"
-                )
-                while (any.contains(".")) {
-                    index += 1
-                    allow = StringUtils.equalsIgnoreCase(allowSuffix[index], suffix)
-                    if (allow) {
-                        break
-                    }
-                }
+                if (StringUtils.equalsIgnoreCase("AVI", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("FLV", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("MOV", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("MKV", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("MP4", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("MPEG", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("MPG", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("VOB", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("WMV", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("3GP", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("ASF", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("DIVX", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("F4V", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("M4V", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("WEBM", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("MXF", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("OGG", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("TS", suffix)) {
+                    true
+                } else if (StringUtils.equalsIgnoreCase("M2TS", suffix)) {
+                    true
+                } else StringUtils.equalsIgnoreCase("SWF", suffix)
             }
-            return allow
+
         }
     }
 }
